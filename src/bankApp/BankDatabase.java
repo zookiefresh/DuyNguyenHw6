f2837746b9a7b6f749d1c86911342b399bfc5789
Duy Nguyen <salesnguyen1@gmail.com>
2015-02-15 17:27:04 -0800

    Complete bankdatabase class.

--- src/bankApp/BankDatabase.java
+++ src/bankApp/BankDatabase.java
@@ -11,24 +11,34 @@ package bankApp;
  */
 public class BankDatabase {
     
+    private BankAccount[] allAccounts = new BankAccount[5];
+    private int numAccounts = -1;
+    
     BankDatabase(){
         
     }
     
-    void createCheckingAccount(String customerName, String ssn, float deposit){
-        
+    void createCheckingAccount(String customerName, String ssn, float deposit){                
+        String[] name = customerName.split(" ");        
+        numAccounts = numAccounts + 1;
+        allAccounts[numAccounts] = new CheckingAccount(name[0], name[1], ssn, deposit);        
     }
     
     void createSavingAccount(String customerName, String ssn, float deposit){
-        
+        String[] name = customerName.split(" ");        
+        numAccounts = numAccounts + 1;
+        allAccounts[numAccounts] = new SavingsAccount(name[0], name[1], ssn, deposit);                
     }
     
     void print(){
-        
+        for(int i = 0 ; i <= numAccounts; i++){
+            allAccounts[i].checkBalance();
+        }
     }
     
     void applyInterest(){
-        
+        for(int i = 0 ; i <= numAccounts; i++){
+            allAccounts[i].applyInterest();
+        }
     }    
-    
 }
